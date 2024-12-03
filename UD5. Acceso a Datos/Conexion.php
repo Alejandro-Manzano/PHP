2b49cb7485649c5b621915eaa26163aa7853cf85
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2024-12-03 09:18:58 +0100

    queryes

--- UD5. Acceso a Datos/Conexion.php
+++ UD5. Acceso a Datos/Conexion.php
@@ -8,8 +8,38 @@
 
     if (mysqli_connect_errno()) {
         echo "Ha fallado la conexion: ". mysqli_connect_error();
+        exit();
     }
 
     echo "<h1>Bienvenido a MYSql</h1>";
+
+
+    //Consulta a pasajero
+    $consulta = "SELECT * FROM `pasajero`";
+    $listaPasajeros = mysqli_query($conexion, $consulta);
+
+    if ($listaPasajeros) {
+    echo "<h2>Datos pasajero</h2>"; 
+        foreach ($listaPasajeros as $pasajero) {
+            echo "Id: {$pasajero['idpasajero']}<br>";
+            echo "Nombre: {$pasajero['nombre']}<br>";
+            echo "Apellido paterno: {$pasajero['apaterno']}";
+        }
+
+    }
+
+    $consulta2 = "SELECT * FROM `vuelo`";
+    $listaVuelos = mysqli_query($conexion, $consulta2);
+
+    if ($listaVuelos) {
+        echo "<h2>Datos vuelos</h2>"; 
+            foreach ($listaVuelos as $vuelo) {
+                echo "Id: {$vuelo['idvuelo']}<br>";
+                echo "Plazas: {$vuelo['n_plazas']}<br>";
+                echo "Disponible?: {$vuelo['disponible']}<br>";
+                echo "Pais: {$vuelo['idpais']}<br>";
+            }
+    
+        }
 
 ?>
